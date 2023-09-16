6334170b723b1d1907a31269817e658cb34227eb
Rajat <rs.sunny100@gmail.com>
2023-09-16 18:03:33 +0530

    Code push

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.driver.model.Driver;
 import com.driver.repository.DriverRepository;
 
+import java.util.Optional;
+
 @Service
 public class DriverServiceImpl implements DriverService {
 
@@ -22,17 +24,37 @@ public class DriverServiceImpl implements DriverService {
 	public void register(String mobile, String password){
 		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
 
+		Driver driver = new Driver();
+		driver.setMobile(mobile);
+		driver.setPassword(password);
+
+		Cab cab = new Cab();
+		cab.setAvailable(true);
+		cab.setPerKmRate(10);
+
+		driver.setCab(cab);
+		driverRepository3.save(driver);
 	}
 
 	@Override
 	public void removeDriver(int driverId){
 		// Delete driver without using deleteById function
-
+		driverRepository3.deleteById(driverId);
 	}
 
 	@Override
 	public void updateStatus(int driverId){
 		//Set the status of respective car to unavailable
+
+		Optional<Driver> optionalDriver = driverRepository3.findById(driverId);
+
+		if(optionalDriver.isPresent()){
+			Driver driver = optionalDriver.get();
+			driver.getCab().setAvailable(false);
+
+			driverRepository3.save(driver);
+		}
+
 
 	}
 }
